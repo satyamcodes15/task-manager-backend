f9e692f0136a85d82e24d86a6412365b356d498d
Satyamcodes15 <satyamkachhi65@gmail.com>
2026-03-20 15:45:36 +0530

    fix port issue

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.get("/", (req, res) => {
     res.send("API Running");
 });
 app.use("/tasks", require("./routes/taskRoutes"));
-app.listen(process.env.PORT, () => {
-    console.log("Server running on port 5000");
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
